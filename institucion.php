b82ec784249166182195ce12148b0e2a1fc1a5d2
sukkoy298 <carlosaug47@gmail.com>
2025-01-02 10:02:50 -0400

    Update institucion.php

--- institucion.php
+++ institucion.php
@@ -5,21 +5,62 @@ class instituto{
     public $ciudad;
     public $representante;
     public $correo;
-    public $contrasena;
+    public $mensaje;
 
     //cosntructor
-    public function __construct($nombre, $ciudad, $representante, $correo, $contrasena) {
+    public function __construct($nombre, $ciudad, $representante, $correo, $mensaje) {
         $this -> nombre= $nombre;
         $this -> ciudad = $ciudad;
         $this -> representante = $representante;
         $this -> correo = $correo;
-        $this -> contrasena = $contrasena;
+        $this -> mensaje = $mensaje;
     }
 
     //metodos
     public function registrar(){
         
     }
+
+    public function getNombre(){
+        return $this -> nombre;
+    }
+
+    public function getCiudad(){
+        return $this -> ciudad;
+    }
+
+    public function getRepresentante(){
+        return $this -> representante;
+    }
+
+    public function getCorreo(){
+        return $this -> correo;
+    }
+
+    public function getMensaje(){
+        return $this -> mensaje;
+    }
+
+    public function setNombre($nombre){
+        $this -> nombre = $nombre;
+    }
+
+    public function setCiudad($ciudad){
+        $this -> ciudad = $ciudad;
+    }
+
+    public function setRepresentante($representante){
+        $this -> representante = $representante;
+    }
+
+    public function setCorreo($correo){
+        $this -> correo = $correo;
+    }
+
+    public function setMensaje($mensaje){
+        $this -> mensaje = $mensaje;
+    }
+
 }
 
 ?>
