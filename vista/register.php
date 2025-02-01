6f59d4f775ed3ab7eaecc822a12d196110f8466b
sukkoy298 <carlosaug47@gmail.com>
2025-01-31 21:25:43 -0400

    feat: update registration form to improve user experience and add password field

--- vista/register.php
+++ vista/register.php
@@ -10,39 +10,27 @@
 
 <body>
     <div class="container mt-5">
-        <h2 class="text-center">Registro de Institución</h2>
+        <h2 class="text-center">Registro Usuario</h2>
         <form action="../controlador/registroControler.php" method="POST" class="mt-4">
             <div class="form-group">
                 <label for="nombrePersona">Nombre:</label>
-                <input type="text" id="nombrePersona" name="nombrePersona" class="form-control" required>
+                <input type="text" id="nombrePersona" name="nombrePersona" class="form-control" placeholder="Nombre de usuario..." required>
             </div>
             <div class="form-group">
                 <label for="empresa">Empresa:</label>
-                <input type="text" id="empresa" name="empresa" class="form-control" placeholder="Nombre de la empresa o institucion educativa" required>
+                <input type="text" id="empresa" name="empresa" class="form-control" placeholder="Nombre de la empresa o institucion educativa..." required>
             </div>
             <div class="form-group">
                 <label for="ciudad">Ciudad:</label>
                 <input type="text" id="ciudad" name="ciudad" class="form-control" required>
             </div>
             <div class="form-group">
-                <label for="tipoSistema">Tipo de Sistema</label>
-                <select class="form-control" id="tipoSistema" name="tipoSistema" required>
-                    <option value="">Seleccione un tipo</option>
-                    <option value="evaluacion">Evaluación</option>
-                    <option value="aula_virtual">Aula Virtual</option>
-                    <option value="servicio_mensajeria">Servicio de Mensajería</option>
-                    <option value="sae_pagos">SAE Pagos</option>
-                    <option value="asistencia_tecnica">Asistencia Técnica</option>
-                    <option value="migracion_datos">Migración de Datos</option>
-                </select>
-            </div>
-            <div class="form-group">
                 <label for="correoElectronico">Correo Electronico:</label>
                 <input type="email" id="correoElectronico" name="correoElectronico" class="form-control" required>
             </div>
             <div class="form-group">
-                <label for="descripcion">Descripción</label>
-                <textarea class="form-control" id="descripcion" name="descripcion" rows="3" required></textarea>
+                <label for="password">Contraseña</label>
+                <input type="password" class="form-control" id="password" name="password" required></textarea>
             </div>
             <button type="submit" class="btn btn-primary btn-block">Registrar</button>
         </form>
